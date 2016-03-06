0de6985bb617230ba6142725bb3959e9998f9f88
Joao Dias <diasjoaoac@gmail.com>
2016-03-06 16:28:34 +0000

    changed comments

--- estimate_distance.m
+++ estimate_distance.m
@@ -1,17 +1,17 @@
-%----------------------------------------------------------------------
+%--------------------------------------------------------------------------
 %
-% DESCRIPTION: Estimates the distance between the WiFi Access Point and
-% 			   the unknown position based on the observed WiFi RSS 
-% 			   usign the FSPL (Free Space Path Loss) propagation model
+% DESCRIPTION: Estimates the distance between the WiFi Access Point and the
+%              unknown position based on the observed WiFi RSS usign the 
+%              FSPL (Free Space Path Loss) propagation model
 %
 %      INPUTS: Observed WiFi RSS (related to an access point)
 %
-%	  OUTPUTS: Estimated distance between the unknown position and
-%			   the given access point 
+%	  OUTPUTS: Estimated distance between the unknown position and the 
+%              given access point 
 %
 %  REFERENCES: http://goo.gl/cGXmDw
 %
-%----------------------------------------------------------------------
+%--------------------------------------------------------------------------
 
 function [ d ] = distance( RSS )
 	result = (27.55 - (20 * log10(2400)) + abs(RSS)) / 20;
